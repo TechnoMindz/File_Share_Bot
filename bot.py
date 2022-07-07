f01548eaf23e8c0cd0bf5d62bdb3b7ec570beb7d
Techno Mindz <97782515+TechnoMindz@users.noreply.github.com>
2022-07-07 11:05:27 +0530

    Updated...

--- bot.py
+++ bot.py
@@ -30,7 +30,7 @@ class Bot(Client):
                 self.LOGGER(__name__).warning(a)
                 self.LOGGER(__name__).warning("Bot can't Export Invite link from Force Sub Channel!")
                 self.LOGGER(__name__).warning(f"Please Double check the FORCE_SUB_CHANNEL value and Make sure Bot is Admin in channel with Invite Users via Link Permission, Current Force Sub Channel Value: {FORCE_SUB_CHANNEL}")
-                self.LOGGER(__name__).info("\nBot Stopped. Join https://t.me/TechnoMindzChat for support")
+                self.LOGGER(__name__).info("\nBot Stopped. Join @TechnoMindzChat for support")
                 sys.exit()
         try:
             db_channel = await self.get_chat(CHANNEL_ID)
@@ -40,13 +40,13 @@ class Bot(Client):
         except Exception as e:
             self.LOGGER(__name__).warning(e)
             self.LOGGER(__name__).warning(f"Make Sure bot is Admin in DB Channel, and Double check the CHANNEL_ID Value, Current Value {CHANNEL_ID}")
-            self.LOGGER(__name__).info("\nBot Stopped. Join https://t.me/TechnoMindzChat for support")
+            self.LOGGER(__name__).info("\nBot Stopped. Join @TechnoMindzChat for support")
             sys.exit()
 
         self.set_parse_mode("html")
-        self.LOGGER(__name__).info(f"Bot Running..!\n\nCreated by Techno Mindz\nhttps://t.me/technomindzchat")
+        self.LOGGER(__name__).info(f"Bot Running..!\n\nCreated by <a href=https://t.me/Technomindzchat><b><i>★彡[Techno Mindz]彡★</i></b></a>")
         self.username = usr_bot_me.username
 
     async def stop(self, *args):
         await super().stop()
-        self.LOGGER(__name__).info("Bot stopped.")
+        self.LOGGER(__name__).info("Bot stopped...")
